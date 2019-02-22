24c8d1d4dedd0ea99647573a7c9901202687a5b0
Divyaraj Bakrola <divyaraj@sfs-wifi-dhcp-10-142-10-87.wifi.sfsu.edu>
2019-02-21 18:54:15 -0800

    templating engine added

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res)=> {
        
-     res.send("Yeh hone wala hai about page. jabarjast")
+     res.render('about.ejs')
 })
 
 
