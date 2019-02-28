31621f1b06d43c01ecee0dbc1d58d9bedd186618
Ubuntu <ubuntu@ip-172-31-33-70.us-east-2.compute.internal>
2019-02-28 19:54:02 +0000

    Installed create-react-app frontend project structure and adjusted app to app communication ports and proxy

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
 const express = require('express')
 const app = express()
+const axios = require('axios')
+const path = require('path')
 
-app.set('view engine', 'ejs')
+// Use path to access React App files
+app.use(express.static(path.join(__dirname, 'client/build')));
 
+// Get Method, REACT app port 3001
 app.get('/', (req, res)=> {
-       
-     res.render('about.ejs')
-})
 
+	axios.get('http://localhost:3001/',
+		{headers: {
+       		 'Content-Type': 'application/json;charset=UTF-8',
+        	 "Access-Control-Allow-Origin": "*"
+      		}
+	})
+	.then((networkResponse) => {
+	     res.send(networkResponse.data);
+	})
+	.catch((e) => {
+	     res.send(':( did not work.');
+	     console.log(e);
+	});
+});
 
-
-
-
-
-
-
-
-
-
+// NODE Express app running on port 3000 if not environment port
 app.listen(process.env.PORT || 3000, ()=> {
-       
       console.log("server runnning ")
 })
